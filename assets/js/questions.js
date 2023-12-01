1c9eda413ac39a74e4082ba9f4e172f571d46445
Nikola84ca <nicolateotorriatte@outlook.com>
2023-12-01 20:31:23 +0000

    added an array of object containing the questions and answers in questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,25 @@
+// Array of Objects for the questions
+
+let questions = [
+    {
+      questionText: "What is JavaScript?",
+      answers: ["A pastry shop", "A Chinese Restaurant", "A programming language", "An elicopter"],
+      correctAnswer: "A programming language"
+    },
+    {
+      questionText: "What does JavaScript use loops for?",
+      answers: ["To wash dishes", "Fry potatoes", "Implement a series of instructions and comparisons", "update the colors of the background of your desktop"],
+      correctAnswer: "Implement a series of instructions and comparisons"
+    },
+    {
+      questionText: "What happens if you don't declare a variable in JS?",
+      answers: ["The code won't work", "A puppy cries", "Your PC will restart", "Your mouse will run away"],
+      correctAnswer: "The code won't work"
+    },
+    {
+     questionText: "What does i++ do?",
+     answers: ["Creates a new object", "Increments the value of i in a loop", "Checks for bugs", "Makes your code run faster"],
+     correctAnswer: "Increments the value of i in a loop"
+      },
+  ];
+  
