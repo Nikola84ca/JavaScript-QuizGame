bc96bee536962fe3cc5a99dfb8cc0d97a97502e1
Nikola84ca <nicolateotorriatte@outlook.com>
2023-12-04 17:20:54 +0000

    Implemented the questions structure and the loop to remove 10 secs from the timer when a wrong answer is selected

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,21 +3,124 @@ let timer = 75; // Initial time in seconds
 // Display the initial time
 document.getElementById("time").textContent = timer;
 
-// Set up an interval to update the timer every second
-let timerInterval = setInterval(function () {
-    // Update the timer
-    timer--;
+// Find the "Start Quiz" button by its id
+let startButton = document.getElementById("start");
 
-    // Display the updated time
-    document.getElementById("time").textContent = timer;
+// Add an event listener to start the quiz when the button is clicked
+startButton.addEventListener("click", startQuiz);
 
-    // Check if the timer has reached 0
-    if (timer <= 0) {
-        clearInterval(timerInterval); // Stop the interval
-        // Additional logic when the timer reaches 0 (e.g., endQuiz())
+function startQuiz() {
+    // Hide the start screen
+    document.getElementById("start-screen").classList.add("hide");
+    
+    // Show the questions div
+    document.getElementById("questions").classList.remove("hide");
+
+    // Start the timer interval
+    let timerInterval = setInterval(function () {
+        // Update the timer
+        timer--;
+
+        // Display the updated time
+        document.getElementById("time").textContent = timer;
+
+        // Check if the timer has reached 0
+        if (timer == 0) {
+            clearInterval(timerInterval); // Stop the interval
+            // Additional logic when the timer reaches 0 (e.g., endQuiz())
+        }
+    }, 1000);
+
+    // Display the first question
+    displayQuestion(questions[0]);
+}
+
+// Function to display a question
+function displayQuestion(question) {
+    // Display the question text
+    document.getElementById("question-title").textContent = question.questionText;
+
+    // Display answer options as buttons
+    let choicesContainer = document.getElementById("choices");
+    choicesContainer.innerHTML = "";
+
+    for (let j = 0; j < question.answers.length; j++) {
+        let choiceBtn = document.createElement("button");
+        choiceBtn.textContent = question.answers[j];
+        choiceBtn.setAttribute("data-index", j); // Set data-index to identify the chosen answer
+        choiceBtn.addEventListener("click", handleAnswerClick); // Add click event listener
+        choicesContainer.appendChild(choiceBtn);
     }
-}, 1000); 
+}
 
+// Function to handle the user's answer click
+function handleAnswerClick(event) {
+    let userAnswerIndex = parseInt(event.target.getAttribute("data-index"), 10);
+    checkAnswer(userAnswerIndex);
+}
+
+
+let currentQuestionIndex = 0; // Track the index of the current question
+let score = 0; // Initialize the score
+
+// Function to check the user's answer
+function checkAnswer(userAnswerIndex) {
+    let currentQuestion = questions[currentQuestionIndex];
+
+    // Check if the user's answer is correct
+    if (userAnswerIndex === currentQuestion.answers.indexOf(currentQuestion.correctAnswer)) {
+        // Correct answer
+        score += 10; // Increment the score
+    } else {
+        // Incorrect answer
+        timer -= 10; // Deduct 10 seconds from the timer
+        if (timer < 0) {
+            timer = 0; // Ensure the timer doesn't go below 0
+        }
+    }
+
+    // Move to the next question or end the quiz
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex < questions.length) {
+        // Display the next question
+        displayQuestion(questions[currentQuestionIndex]);
+    } else {
+        // No more questions, end the quiz
+        endQuiz();
+    }
+}
+
+// Function to end the quiz
+function endQuiz() {
+    // Stop the timer interval
+    clearInterval(timerInterval);
+
+    // Hide the questions div
+    document.getElementById("questions").classList.add("hide");
+
+    // Display the end screen
+    let endScreen = document.getElementById("end-screen");
+    endScreen.classList.remove("hide");
+
+    // Display the final score
+    document.getElementById("final-score").textContent = score;
+}
+
+// Additional code to handle the submission of initials (you may need to adjust this based on your specific requirements)
+let submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", submitScore);
+
+function submitScore() {
+    let initials = document.getElementById("initials").value;
+
+    // Logic to handle saving the score and initials (you can implement this part)
+    // For example, you can store it in local storage or send it to a server.
+    
+    // After saving, you might want to redirect the user or perform other actions.
+    // For now, let's assume you have a function saveScore(initials, score)
+    saveScore(initials, score);
+}
 
 // if the answer is wrong remove time from timer
 
